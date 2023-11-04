90b4d1833939bfe28a72a7610c8c293f809c20fd
Shashank Mishra <95490636+shashank2232@users.noreply.github.com>
2023-11-04 11:58:10 +0530

    Delete 5. REST API SECURITY directory

--- 5. REST API SECURITY/1. springsecurity/src/main/java/com/example/springboot/employeedemo/daorepo/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.employeedemo.daorepo;
-
-import com.example.springboot.employeedemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
